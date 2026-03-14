435f5d8e353c5f9a47dea66ad2c0ae1c889c8fad
Abdulaziz <abdulaziz.gulomov91@gmail.com>
2026-03-14 20:37:02 +0500

    lanaguage fix

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from apps import accounts
+
 
 class Profession(models.Model):
     name = models.CharField(max_length=100, unique=True)
